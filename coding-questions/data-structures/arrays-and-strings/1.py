8765e19777dac6fedbd606b7b01511e6af5ee31b
Dennis Ideler <ideler.dennis@gmail.com>
2014-08-24 12:54:45 +0100

    Add tests and benchmarks

--- coding-questions/data-structures/arrays-and-strings/1.py
+++ coding-questions/data-structures/arrays-and-strings/1.py
@@ -1,3 +1,5 @@
+from timeit import timeit
+
 def contains_unique_chars(string):
     char_set = set()
     for character in string:
@@ -10,3 +12,43 @@ def is_unique(s):
     """Simpler code and more efficient than the above."""
     return len(s) == len(set(s))
 
+def assert_equal(expected, actual):
+    """Basic equality assertion. Prints PASS or FAIL."""
+    result = 'FAIL'
+
+    if expected == actual:
+        result = 'PASS'
+
+    print('{}: {} -> {}'.format(result, expected, actual))
+
+if __name__ == "__main__":
+    method_a = "contains_unique_chars"
+    method_b = "is_unique"
+    setup_a = "from __main__ import contains_unique_chars"
+    setup_b = "from __main__ import is_unique"
+    print(method_a,
+          timeit(method_a + "('abc')",
+          setup=setup_a))
+    print(method_b,
+          timeit(method_b + "('abc')",
+          setup=setup_b))
+    print(method_a,
+          timeit(method_a + "('abcdefghijklmnopqrstuvxwyz1234567890')",
+          setup=setup_a))
+    print(method_b,
+          timeit(method_b + "('abcdefghijklmnopqrstuvxwyz1234567890')",
+          setup=setup_b))
+
+    tests = [
+                ('hello', False),
+                ('Dennis', False),
+                ('', True),
+                ('world', True),
+                ('  ', False),
+                ('HAHA!', False),
+                ('Hypem', True)
+            ]
+
+    for test in tests:
+        assert_equal(contains_unique_chars(test[0]), test[1])
+        assert_equal(is_unique(test[0]), test[1])
